80e1073a1d86b9d184dd2e93994d1b5bfc97d505
frontwebdev <nerzhmet@gmail.com>
2017-08-02 17:51:53 +0300

    Create xhrspy.js

---
+++ xhrspy.js
@@ -0,0 +1,21 @@
+var tempGlobal;
+
+function addXMLRequestCallback(callback) {
+    var oldSend, i;
+    if (XMLHttpRequest.callbacks) {
+        XMLHttpRequest.callbacks.push(callback);
+    } else {
+        XMLHttpRequest.callbacks = [callback];
+        oldSend = XMLHttpRequest.prototype.send;
+        XMLHttpRequest.prototype.send = function() {
+            for (i = 0; i < XMLHttpRequest.callbacks.length; i++) {
+                XMLHttpRequest.callbacks[i](this);
+            }
+            oldSend.apply(this, arguments);
+        }
+    }
+}
+
+addXMLRequestCallback(function(xhr) {
+    console.log(xhr);
+});
