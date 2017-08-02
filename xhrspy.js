934342987a9a5a99fe82e446975abe2f9cf9d39b
frontwebdev <nerzhmet@gmail.com>
2017-08-02 18:06:33 +0300

    Update xhrspy.js

--- xhrspy.js
+++ xhrspy.js
@@ -16,6 +16,24 @@ function addXMLRequestCallback(callback) {
     }
 }
 
+function getParameterByName(name, url) {
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 addXMLRequestCallback(function(xhr) {
     console.log(xhr);
+    
+    if (!xhr || !xhr.responseURL) return;
+    if ((new URL(url)).hostname !== 'www.google-analytics.com') return;
+    
+    var ecommerceFlag = getParameterByName('ec', xhr.responseURL);
+    if (!ecommerceFlag || ecommerceFlag !== 'ecommerce') return;
+    var orderId = getParameterByName('ti', xhr.responseURL);
+    
+    console.log('order id  is ', orderId);
 });
