1e8b875beae67ef3846d42b92c81528df778f029
frontwebdev <nerzhmet@gmail.com>
2017-08-03 15:06:06 +0300

    Update dudochnik.js

--- dudochnik.js
+++ dudochnik.js
@@ -1,20 +1,7 @@
-var imgc = Image.prototype.constructor;
-Image = function () {
-    imgc.apply(this, arguments);
-    console.log ('ccc', this, this instanceof Image);
-    var self = this;
-
-    this.addEventListener ("load", function () {
-        alert ('yes');
-    }, false);
-};
-
-
-
-//Object.defineProperty(HTMLImageElement.prototype, 'onload', {
-  //configurable: true,
-  //enumerable: true,
-  //value: function () {
-    //console.log(this, "loaded");
-  //}
-//});
+Object.defineProperty(HTMLImageElement.prototype, 'onload', {
+  configurable: true,
+  enumerable: true,
+  value: function () {
+    console.log(this, "loaded");
+  }
+});
