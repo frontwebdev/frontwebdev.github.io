d624a5b5a4d005029fe60e6161744c9e378d22ac
frontwebdev <nerzhmet@gmail.com>
2017-08-03 13:59:05 +0300

    Update dudochnik.js

--- dudochnik.js
+++ dudochnik.js
@@ -1,11 +1,19 @@
-
-if (typeof gaplugins != "undefined") {
-  if (typeof gaplugins.EC == "function") {
-    console.log('bingo')
-  } else {
-    console.log('no ecommerce plugin in GA')
-  }
-   
-} else {
-  console.log('no GA plugins')
+function addImageOnloadCallback(callback) {
+    var oldSend, i;
+    if (Image.callbacks) {
+        Image.callbacks.push(callback);
+    } else {
+        Image.callbacks = [callback];
+        oldImageOnload = Image.prototype.onload;
+        Image.prototype.onload = function() {
+            for (i = 0; i < Image.callbacks.length; i++) {
+                Image.callbacks[i](this);
+            }
+            oldImageOnload.apply(this, arguments);
+        }
+    }
 }
+
+addImageOnloadCallback(function(res) {
+  console.log('image onload', res);
+})
