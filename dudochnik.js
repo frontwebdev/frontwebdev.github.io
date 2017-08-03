ec3a4dc204f6ba8796c9f4d8d192ca8132341c8c
frontwebdev <nerzhmet@gmail.com>
2017-08-03 14:25:59 +0300

    Update dudochnik.js

--- dudochnik.js
+++ dudochnik.js
@@ -1,27 +1,26 @@
 function addImageOnloadCallback(callback) {
     var oldSend, i;
-    if (Image.callbacks) {
-        Image.callbacks.push(callback);
+    if (HTMLImageElement.callbacks) {
+        HTMLImageElement.callbacks.push(callback);
     } else {
-        Image.callbacks = [callback];
-        Image.prototype.onload = function() {
-            for (i = 0; i < Image.callbacks.length; i++) {
-                Image.callbacks[i](this);
+        HTMLImageElement.callbacks = [callback];
+        HTMLImageElement.prototype.onload = function() {
+            for (i = 0; i < HTMLImageElement.callbacks.length; i++) {
+                HTMLImageElement.callbacks[i](this);
             }
-            oldImageOnload.apply(this, arguments);
         }
     }
 }
 
-//addImageOnloadCallback(function(res) {
-  //console.log('image onload', res);
-//});
-
-
-Object.defineProperty(HTMLImageElement.prototype, 'onload', {
-  configurable: true,
-  enumerable: true,
-  value: function () {
-    console.log(this, "loaded");
-  }
+addImageOnloadCallback(function(res) {
+  console.log('image onload', res);
 });
+
+
+//Object.defineProperty(HTMLImageElement.prototype, 'onload', {
+  //configurable: true,
+  //enumerable: true,
+  //value: function () {
+    //console.log(this, "loaded");
+  //}
+//});
